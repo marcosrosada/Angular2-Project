6c18a5c2512245fb98075858d83a1d0283fc0352
Marcos Rosada <ochild@gmail.com>
2017-05-20 22:57:23 -0300

    Two-way Data Binding

--- src/app/binding/binding.module.ts
+++ src/app/binding/binding.module.ts
@@ -1,26 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { ClassStyleBindingComponent } from './class-style-binding/class-style-binding.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [
       InterpolationComponent,
       PropertyBindingComponent,
       ClassStyleBindingComponent,
-      EventBindingComponent
+      EventBindingComponent,
+      TwoWayDataBindingComponent
   ],
   exports: [
       InterpolationComponent,
       PropertyBindingComponent,
       ClassStyleBindingComponent,
-      EventBindingComponent
+      EventBindingComponent,
+      TwoWayDataBindingComponent
   ]
 })
 export class BindingModule { }
