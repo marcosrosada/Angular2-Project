95dd9dddf7028c19d48d72c6c750bcf4ef0d821d
Marcos Rosada <ochild@gmail.com>
2017-05-20 22:51:30 -0300

    Event Binding

--- src/app/binding/binding.module.ts
+++ src/app/binding/binding.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
 import { ClassStyleBindingComponent } from './class-style-binding/class-style-binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 
 @NgModule({
@@ -12,12 +13,14 @@ import { ClassStyleBindingComponent } from './class-style-binding/class-style-bi
   declarations: [
       InterpolationComponent,
       PropertyBindingComponent,
-      ClassStyleBindingComponent
+      ClassStyleBindingComponent,
+      EventBindingComponent
   ],
   exports: [
       InterpolationComponent,
       PropertyBindingComponent,
-      ClassStyleBindingComponent
+      ClassStyleBindingComponent,
+      EventBindingComponent
   ]
 })
 export class BindingModule { }
---
+++ src/app/binding/event-binding/event-binding.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent implements OnInit {
+
+    currentValue: string = '';
+    valueSaved: string = '';
+    isMouseOver: boolean = false;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    onKeyUp(event: KeyboardEvent){
+        this.currentValue = (<HTMLInputElement>event.target).value;
+        console.log(this.currentValue);
+    }
+
+    doSave(value) {
+      this.valueSaved = value;
+    }
+
+    onMouseOver() {
+        this.isMouseOver = !this.isMouseOver;
+    }
+
+}
