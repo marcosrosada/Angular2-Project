3af89d4212d2c7c557ba1b826082f524133240a2
marcos.rosada <marcos.rosada@rustcon.com.br>
2017-05-23 17:41:36 -0300

    ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { BindingModule } from './binding/binding.module';
 import { LifeCicleComponent } from './life-cicle/life-cicle.component';
 import { DomViewChildComponent } from './dom-view-child/dom-view-child.component';
 import { SwitchComponent } from './switch/switch.component';
+import { NgclassComponent } from './ngclass/ngclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LifeCicleComponent,
     DomViewChildComponent,
-    SwitchComponent
+    SwitchComponent,
+    NgclassComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ngclass/ngclass.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngclass',
+  templateUrl: './ngclass.component.html',
+  styleUrls: ['./ngclass.component.css']
+})
+export class NgclassComponent implements OnInit {
+
+    liked: boolean = false;
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    doLike() {
+        this.liked = !this.liked;
+    }
+
+}
