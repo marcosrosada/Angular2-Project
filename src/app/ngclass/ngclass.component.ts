2ec34ed8ac1b511ca4fbf26ae3041f0aa7104b9d
marcos.rosada <marcos.rosada@rustcon.com.br>
2017-05-24 09:47:38 -0300

    ngStyle

--- src/app/ngclass/ngclass.component.ts
+++ src/app/ngclass/ngclass.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NgclassComponent implements OnInit {
 
     liked: boolean = false;
+    cliked: boolean = false;
 
     constructor() { }
 
@@ -16,6 +17,10 @@ export class NgclassComponent implements OnInit {
 
     doLike() {
         this.liked = !this.liked;
+    }
+
+    doChangeStyle() {
+        this.cliked = !this.cliked;
     }
 
 }
