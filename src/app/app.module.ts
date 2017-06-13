5d824133b1549d17fa708fc7984aceb4c60123d0
Marcos Rosada <ochild@gmail.com>
2017-06-12 23:04:18 -0300

    Materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MaterializeModule } from 'angular2-materialize';
+
 import { AppComponent } from './app.component';
 import { CoursesModule } from './courses/courses.module';
 import { BindingModule } from './binding/binding.module';
