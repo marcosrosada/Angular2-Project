48e5081d623cb9035a516691cd0cc6f5d622ddf5
marcos.rosada <marcos.rosada@rustcon.com.br>
2017-06-20 16:59:53 -0300

    Set up router

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CoursesComponent } from './courses/courses.component';
+import { CourseDetailsComponent } from './courses/course-details/course-details.component';
 
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'courses', component: CoursesComponent },
+    { path: 'courses/:id', component: CourseDetailsComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
--- src/app/courses/course-details/course-details.component.ts
+++ src/app/courses/course-details/course-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-details',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+
+  constructor(private route: ActivatedRoute) { 
+    console.log(route);
+    
+  }
 
   ngOnInit() {
   }
