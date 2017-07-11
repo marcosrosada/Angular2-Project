63ab899202fe36a11d4c57f8bae91598bc2de0e7
Marcos Rosada <marcos.rosada@rustcon.com.br>
2017-07-11 17:14:36 -0300

    Query Params

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 
 import { CoursesService } from './courses.service';
 
@@ -10,12 +12,32 @@ import { CoursesService } from './courses.service';
 export class CoursesComponent implements OnInit {
 
   courses: any[];
+  page: number;
+  inscription: Subscription;
 
-  constructor(private courseService: CoursesService) {
+  constructor(
+        private courseService: CoursesService,
+        private route: ActivatedRoute,
+        private router: Router
+        ) {
   }
 
   ngOnInit() {
     this.courses = this.courseService.getCourses();
+
+    this.inscription = this.route.queryParams.subscribe( (queryParams: any) => {
+      this.page = queryParams['page'];
+    });
+  }
+
+  ngOnDestroy() {
+    this.inscription.unsubscribe();
+  }
+
+  nextPage() {
+    this.router.navigate(['/courses'], {
+      queryParams: { 'page': ++this.page }
+    });
   }
 
 }
